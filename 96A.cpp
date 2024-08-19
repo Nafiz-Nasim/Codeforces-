d4d4addbaa625bdacd3707c879b6a35e41a2bd49
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-19 20:54:39 +0600

    Add files via upload

---
+++ 96A.cpp
@@ -0,0 +1,44 @@
+//string controling
+
+
+
+#include<iostream>
+#include<string>
+#include<conio.h>
+using namespace std;
+int main()
+{
+string number;
+int i,count=0;
+ cin>>number;
+ for (i=0;i<number.size();i++)
+ {
+   if(number[i]==number[i+1])
+   {
+    count++;
+    if(count>6)
+    {
+        break;
+    }
+   }
+   else{
+    count=0;
+   }
+ }
+ if(count>6)
+ {
+    cout<<"YES"<<endl;
+ }
+ else{
+    cout<<"NO"<<endl;
+ }
+
+
+
+
+
+
+
+    getchar();
+
+}
