bf12c671e3fc81d25f9ec2f9af0e149553fed34f
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-25 19:59:44 +0600

    ICPC Assiut University Community

---
+++ Summation from 1 to N.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+ 
+    long long int sum=0;
+     long long int N;
+    cin>>N;
+    for( long long int i=1;i<=N;i++)
+    {
+       sum=sum+i;
+    }
+    cout<<sum;
+    
+    return 0;
+}
+ 
+ 
+ 
+ 
