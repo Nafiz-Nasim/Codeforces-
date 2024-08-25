a491448c1170a07ddc4a8bfa72a2fd7e681a8ba5
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-25 12:37:09 +0600

    ICPC Assiut University Community  contest: Sheet #1 (Data type - Conditions), problem: (D)

---
+++ Difference.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+    long long int A,B,C,D,output;
+    
+    cin >> A >> B>> C>>D;
+    output=((A*B)-(C*D));
+    cout << "Difference = "<< output<< endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
