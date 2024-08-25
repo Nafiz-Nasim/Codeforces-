06d622ec41581da1d69c011f881e7256fdc71b73
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-25 19:52:29 +0600

    ICPC Assiut University Community

---
+++ Digits Summation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+ 
+    long long int n,m;
+    int dn,dm,sum;
+    cin>>n>>m;
+    dn=n%10;
+    dm=m%10;
+    sum=dn+dm;
+    cout<<sum;
+    
+    return 0;
+}
+ 
+ 
+ 
+ 
