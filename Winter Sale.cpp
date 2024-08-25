7ac59b41c47d199db8b5dacfbf66746820cad9fc
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-25 11:04:37 +0600

    Create Winter Sale.cpp
      ICPC Assiut University Community //Contest #1, problem: (A)

---
+++ Winter Sale.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<conio.h>
+ 
+using namespace std;
+int main()
+{
+int discount,discount_price;
+float real_price;
+cin>>discount>>discount_price;
+real_price= discount_price / (1-(discount/100.0));
+cout<<real_price;
+ 
+ 
+return 0;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
