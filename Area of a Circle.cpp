879f2cc9bc914aa92d825679290770d29740a4da
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-25 19:44:32 +0600

    ICPC Assiut University Community

---
+++ Area of a Circle.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+ 
+    const double pi=3.141592653;
+    double R,area;
+    cin>>R;
+ area=pi*R*R;
+ cout<<fixed;
+ cout<<setprecision(9)<<area;
+
+    return 0;
+}
+ 
+ 
+ 
+ 
