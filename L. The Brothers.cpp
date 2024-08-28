bdf4b679ef9bb95e7d0e66d8ffc63864ad2ce71d
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-28 21:20:14 +0600

    ICPC Assiut University Community

---
+++ L. The Brothers.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+ 
+string name1_part1, name1_part_2,name2_part1,name2_part_2;
+cin>>name1_part1>>name1_part_2;
+cin>>name2_part1>>name2_part_2;
+if(name1_part_2==name2_part_2) cout<<"ARE Brothers";
+else cout<<"NOT";
+
+    return 0;
+}
+ 
+ 
+ 
+ 
