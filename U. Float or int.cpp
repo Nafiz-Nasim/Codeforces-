8d8d3eb9f9cd3673115846f5693b1d70701f1426
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-08-28 16:16:10 +0600

    U. Float or int.cpp
    ICPC Assiut University Community

---
+++ U. Float or int.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> 
+using namespace std;
+int main()
+{
+double a,fa;
+
+ cin>>a;
+int b;
+b=a;
+if(a==b)
+{ cout<<"int"<<" "<<a<<endl;
+}
+else
+{
+fa=a-b;
+cout<<"float"<<" "<<b<<" "<<fa;
+}
+
+    return 0;
+}
+ 
