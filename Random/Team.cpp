e7fb26cc09189082143e538487954eacab7a002b
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-12-06 16:23:57 +0600

    Add files via upload

---
+++ Random/Team.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test, count = 0;
+    cin>>test;
+    while (test--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a == 1 & b == 1) || (b == 1 & c == 1)||(c == 1 & a == 1)) count++;
+    }
+    cout << count;
+    return 0;
+}
