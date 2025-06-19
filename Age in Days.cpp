e7a1256b789ab2970b2ab5928912b6903870ae01
A. K. M. Nafiz Ibn Nasim <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-06-19 19:12:19 +0600

    Update Age in Days.cpp

--- Age in Days.cpp
+++ Age in Days.cpp
@@ -4,6 +4,7 @@ int main()
 {
  int input,months,days,years,dm;
  cin>>input;
+ 
 years=input/365;
 cout<<years<<" "<<"years"<<endl;
 dm=input%365;
@@ -11,6 +12,9 @@ months=dm/30;
 cout<<months<<" "<<"months"<<endl;
 days=dm%30;
 cout<<days<<" "<<"days"<<endl;
+
+
+ 
     return 0;
 }
  
